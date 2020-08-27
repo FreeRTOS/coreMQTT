3cbdd26c213d09d0fe055efbb14323e03eff7df7
Archit Aggarwal <architag@amazon.com>
2020-08-27 22:26:57 +0000

    Update file path in mqttFilePaths.cmake

--- mqttFilePaths.cmake
+++ mqttFilePaths.cmake
@@ -7,18 +7,18 @@
 
 # MQTT library source files.
 set( MQTT_SOURCES
-     "${MODULES_DIR}/standard/mqtt/src/mqtt.c"
-     "${MODULES_DIR}/standard/mqtt/src/mqtt_state.c" )
+     "${MODULES_DIR}/standard/mqtt/source/mqtt.c"
+     "${MODULES_DIR}/standard/mqtt/source/mqtt_state.c" )
 
 # Lightweight MQTT library source files.
 set( LIGHTWEIGHT_MQTT_SOURCES
-     "${MODULES_DIR}/standard/mqtt/src/mqtt_lightweight.c" )
+     "${MODULES_DIR}/standard/mqtt/source/mqtt_lightweight.c" )
 
 # MQTT library Public Include directories.
 set( MQTT_INCLUDE_PUBLIC_DIRS
-     "${MODULES_DIR}/standard/mqtt/include"
+     "${MODULES_DIR}/standard/mqtt/source/include"
      "${PLATFORM_DIR}/include" )
 
 # MQTT library Private Include directories.
 set( MQTT_INCLUDE_PRIVATE_DIRS
-     "${MODULES_DIR}/standard/mqtt/src" )
+     "${MODULES_DIR}/standard/mqtt/source" )
