f67f576cd142b8dd3ddee4a8e3e20fe2e757e401
Archit Aggarwal <architag@amazon.com>
2020-08-28 21:23:40 +0000

    Modularize Cmock build configuration to expose cmock targets only if submodule is cloned

--- test/cmock_build.cmake
+++ test/cmock_build.cmake
@@ -1,41 +1,57 @@
-# Build Configuration for CMock and Unity libraries.
-list( APPEND CMOCK_INCLUDE_DIRS
-        "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/src/"
-        "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/extras/fixture/src"
-        "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/extras/memory/src"
-        "${MODULE_ROOT_DIR}/test/CMock/src"
-    )
+# Macro utility to clone the CMock submodule.
+macro( clone_cmock )
+        find_package( Git REQUIRED )
+        message( "Cloning submodule CMock." )
+        execute_process( COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive ${MODULE_ROOT_DIR}/test/CMock
+                        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                        RESULT_VARIABLE CMOCK_CLONE_RESULT )
 
-add_library(cmock STATIC
-    "${MODULE_ROOT_DIR}/test/CMock/src/cmock.c"
-    )
+        if( NOT ${CMOCK_CLONE_RESULT} STREQUAL "0" )
+                message( FATAL_ERROR "Failed to clone CMock submodule." )
+        endif()
+endmacro()
 
-set_target_properties(cmock PROPERTIES
-        ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib
-        POSITION_INDEPENDENT_CODE ON
-        COMPILE_FLAGS "-Og"
+# Macro utility to add library targets for Unity and CMock to build configuration.
+macro( add_cmock_targets )
+        # Build Configuration for CMock and Unity libraries.
+        list( APPEND CMOCK_INCLUDE_DIRS
+                "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/src/"
+                "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/extras/fixture/src"
+                "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/extras/memory/src"
+                "${MODULE_ROOT_DIR}/test/CMock/src"
         )
 
-target_include_directories(cmock PUBLIC
-        ${MODULE_ROOT_DIR}/test/CMock/src
-        ${MODULE_ROOT_DIR}/test/CMock/vendor/unity/src/
-        ${MODULE_ROOT_DIR}/test/CMock/examples
-        ${CMOCK_INCLUDE_DIRS}
+        add_library(cmock STATIC
+        "${MODULE_ROOT_DIR}/test/CMock/src/cmock.c"
         )
 
-add_library(unity STATIC
-        "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/src/unity.c"
-        "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/extras/fixture/src/unity_fixture.c"
-        "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/extras/memory/src/unity_memory.c"
-        )
+        set_target_properties(cmock PROPERTIES
+                ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib
+                POSITION_INDEPENDENT_CODE ON
+                COMPILE_FLAGS "-Og"
+                )
 
-set_target_properties(unity PROPERTIES
-        ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib
-        POSITION_INDEPENDENT_CODE ON
-        )
+        target_include_directories(cmock PUBLIC
+                ${MODULE_ROOT_DIR}/test/CMock/src
+                ${MODULE_ROOT_DIR}/test/CMock/vendor/unity/src/
+                ${MODULE_ROOT_DIR}/test/CMock/examples
+                ${CMOCK_INCLUDE_DIRS}
+                )
 
-target_include_directories(unity PUBLIC
-        ${CMOCK_INCLUDE_DIRS}
-        )
+        add_library(unity STATIC
+                "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/src/unity.c"
+                "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/extras/fixture/src/unity_fixture.c"
+                "${MODULE_ROOT_DIR}/test/CMock/vendor/unity/extras/memory/src/unity_memory.c"
+                )
 
-target_link_libraries(cmock unity)
+        set_target_properties(unity PROPERTIES
+                ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib
+                POSITION_INDEPENDENT_CODE ON
+                )
+
+        target_include_directories(unity PUBLIC
+                ${CMOCK_INCLUDE_DIRS}
+                )
+
+        target_link_libraries(cmock unity)
+endmacro()
