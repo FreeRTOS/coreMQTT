121b93f51cfc344e3b19f715b46e4a95478180fc
Sarena Meas <sarem@amazon.com>
2020-09-09 01:06:50 -0700

    Add documentation for MQTT_DO_NOT_USE_CUSTOM_CONFIG

--- source/include/core_mqtt_config_defaults.h
+++ source/include/core_mqtt_config_defaults.h
@@ -170,4 +170,16 @@
     #define LogDebug( message )
 #endif
 
+/* This macro definition is for Doxygen documentation only. */
+/**
+ * @brief Define this macro to build the MQTT library without the custom config
+ * file core_mqtt_config.h.
+ * 
+ * If a custom config is provided, then MQTT_DO_NOT_USE_CUSTOM_CONFIG should not
+ * be defined.
+ */
+#ifdef DOXYGEN
+    #define MQTT_DO_NOT_USE_CUSTOM_CONFIG
+#endif
+
 #endif /* ifndef CORE_MQTT_CONFIG_DEFAULTS_H_ */
