cafc65874068330656bfa0db1a1137d683a5e8b3
Oscar Abrina <abrinao@amazon.com>
2020-12-06 10:29:37 -0800

    Check if build checks also verify logging
    Use CMAKE_CURRENT_LIST_DIR
    
    Add to real_include_directories, remove from test_include_directories
    
    Update lexicon.txt

--- test/unit-test/core_mqtt_config.h
+++ test/unit-test/core_mqtt_config.h
@@ -30,6 +30,32 @@
 /* Standard include. */
 #include <stdint.h>
 
+/**************************************************/
+/******* DO NOT CHANGE the following order ********/
+/**************************************************/
+
+/* Include logging header files and define logging macros in the following order:
+ * 1. Include the header file "logging_levels.h".
+ * 2. Define the LIBRARY_LOG_NAME and LIBRARY_LOG_LEVEL macros depending on
+ * the logging configuration for MQTT.
+ * 3. Include the header file "logging_stack.h", if logging is enabled for MQTT.
+ */
+
+#include "logging_levels.h"
+
+/* Logging configuration for the MQTT library. */
+#ifndef LIBRARY_LOG_NAME
+    #define LIBRARY_LOG_NAME    "MQTT"
+#endif
+
+#ifndef LIBRARY_LOG_LEVEL
+    #define LIBRARY_LOG_LEVEL    LOG_DEBUG
+#endif
+
+#include "logging_stack.h"
+
+/************ End of logging configuration ****************/
+
 /**
  * @brief Retry count for reading CONNACK from network.
  *
