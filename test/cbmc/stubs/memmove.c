cbaa2cd627397d54c1db6cc23b26e7927f04ca32
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2022-08-19 19:19:51 +0000

    Fix formatting

--- test/cbmc/stubs/memmove.c
+++ test/cbmc/stubs/memmove.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 
-void * memmove ( void * destination, const void * source, size_t num ) {
-  __CPROVER_assert(destination, "memmove destination is nonnull");
-  __CPROVER_assert(source, "memmove source is nonnull");
-  __CPROVER_havoc_object(destination);
-  return destination;
+void * memmove( void * destination,
+                const void * source,
+                size_t num )
+{
+    __CPROVER_assert( destination, "memmove destination is nonnull" );
+    __CPROVER_assert( source, "memmove source is nonnull" );
+    __CPROVER_havoc_object( destination );
+    return destination;
 }
