4d84f05f5511d0b2d76b2f32131724d15f63d3e5
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2022-10-11 23:55:00 +0000

    Fix spellcheck and the formatting

--- test/cbmc/stubs/memmove.c
+++ test/cbmc/stubs/memmove.c
@@ -21,8 +21,8 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
- 
- #include <stdlib.h>
+
+#include <stdlib.h>
 
 void * memmove( void * destination,
                 const void * source,
