a03c173120ca99550bd30e3554f4a4047d646eee
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2023-05-03 18:11:03 -0700

    Update core_mqtt_utest.c

--- test/unit-test/core_mqtt_utest.c
+++ test/unit-test/core_mqtt_utest.c
@@ -4783,7 +4783,7 @@ void test_MQTT_Subscribe_error_paths_timerOverflowCheck( void )
     size_t remainingLength = MQTT_SAMPLE_REMAINING_LENGTH;
     size_t packetSize = MQTT_SAMPLE_REMAINING_LENGTH;
 
-    globalEntryTime = UINT32_MAX - MQTT_OVERFLOW_OFFSET;
+    globalEntryTime = UINT32_MAX;
 
     /* The timer function can be called a maximum of these many times
      * (which is way less than UINT32_MAX). This ensures that if overflow
