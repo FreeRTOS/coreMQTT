8e8c1faee2efb80f4f2ab38b0e3acd83a7683a31
GitHub Action <action@github.com>
2025-04-07 12:00:54 +0000

    Uncrustify: triggered by comment.

--- source/interface/transport_interface.h
+++ source/interface/transport_interface.h
@@ -200,9 +200,9 @@ typedef struct NetworkContext NetworkContext_t;
  * implementation does NOT block.
  * coreMQTT will continue to call the transport interface if it receives
  * a partial packet until it accumulates enough data to get the complete
- * MQTT packet. 
- * A non‐blocking implementation is also essential so that the library's inbuilt 
- * keep‐alive mechanism can work properly, given the user chooses to use 
+ * MQTT packet.
+ * A non‐blocking implementation is also essential so that the library's inbuilt
+ * keep‐alive mechanism can work properly, given the user chooses to use
  * that over their own keep alive mechanism.
  *
  * @param[in] pNetworkContext Implementation-defined network context.
